7b1b755941be4bb70cbd8266cbb45f89e72ddd8f
Jemma  Nzekwe <jemmanzekwe@hotmail.com>
2023-12-05 21:22:04 +0000

    added questions

---
+++ assets/js/questions.js
@@ -0,0 +1,29 @@
+const questions = [
+    {
+        question: "What is the correct way to write a JavaScript comment?",
+        choices: ["<!--This is a comment-->", "//This is a comment", "/*This is a comment*/", "**This is a comment**"],
+        correctAnswer: "/*This is a comment*/"
+    },
+    {
+        question: "Which tag is used to create a hyperlink in HTML?",
+        choices: ["<link>", "<a>", "<href>", "<p>"],
+        correctAnswer: "<a>"
+    },
+    {
+        question: "Which CSS property is used to make text bold?",
+        choices: ["font-weight", "text-style", "bold-text", "text-weight"],
+        correctAnswer: "font-weight"
+    },
+    {
+        question: "Which built-in function is used to print something to the console in JavaScript?",
+        choices: ["print()", "console.log()", "write()", "display()"],
+        correctAnswer: "console.log()"
+    },
+    {
+        question: "What does HTML stand for?",
+        choices: ["Hyper Text Markup Language", "Hyperlinks and Tool-Making Language", "Home Tech Monitoring Language", "House Tech Making Language"],
+        correctAnswer: "Hyper Text Markup Language"
+    },
+
+
+];
