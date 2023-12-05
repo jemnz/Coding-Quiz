16e3c3a12d665c3660df5d35f22ae78c45f58528
Jemma  Nzekwe <jemmanzekwe@hotmail.com>
2023-12-05 21:57:21 +0000

    defined variables

---
+++ assets/js/logic.js
@@ -0,0 +1,8 @@
+const startButton = document.getElementById("start");
+const timeElement = document.getElementById("time");
+const questionTitleElement = document.getElementById("question-title");
+const choicesElement = document.getElementById("choices");
+const feedbackElement = document.getElementById("feedback");
+const initialsInput = document.getElementById("initials");
+const submitButton = document.getElementById("submit");
+
