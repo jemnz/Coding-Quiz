6b602832e0918a13740047c9933e29f95337029b
Jemma  Nzekwe <jemmanzekwe@hotmail.com>
2023-12-05 22:00:15 +0000

    added mcode for quiz initial state

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -6,3 +6,6 @@ const feedbackElement = document.getElementById("feedback");
 const initialsInput = document.getElementById("initials");
 const submitButton = document.getElementById("submit");
 
+let currentQuestionIndex = 0;
+  let timer;
+  let timeLeft = 60; 
